b2ff78570d29802c437dab6a53ae45f74f40fe3c
Michael Leib <133719516+mleib112088@users.noreply.github.com>
2023-05-19 09:11:23 -0400

    Update SQL_Script.sql

--- SQL_Script.sql
+++ SQL_Script.sql
@@ -10,7 +10,19 @@ CTE2 AS (
 	WHERE "Result" = 'Loss'
 	GROUP BY "Result"
 ) 
-SELECT SUM(ml."Boyd_Yards") AS "Boyd Yards", SUM(ml."Higgins_Yards") AS "Higgins Yards", SUM(ml."Chase_Yards") AS "Chase Yards", string_agg((cast(c1."Win" as varchar) || cast(c2."Loss" as varchar)),',')
+SELECT SUM(ml."Boyd_Yards") AS "Boyd Yards", SUM(ml."Higgins_Yards") AS "Higgins Yards", SUM(ml."Chase_Yards") AS "Chase Yards", 
+coalesce(c1."Win", 0) as "Win", coalesce(c2."Loss",0) as "Loss"
+INTO TEMPORARY table mytable
 FROM michael_leib ml
 left JOIN CTE1 c1 ON c1."Result" = ml."Result"  
 left JOIN CTE2 c2 ON c2."Result" = ml."Result" 
+group by 4,5
+
+--execute statement above first, then run statement below
+
+select sum("Boyd Yards") as "Boyd Yards", sum("Higgins Yards") as "Higgins Yards", sum("Chase Yards") as "Chase Yards", (sum("Win") || '-' || sum("Loss")) as "Win/Loss"
+from mytable
+
+--remove temp table by dropping afterwards
+
+--drop table mytable
